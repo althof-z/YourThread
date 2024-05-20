5a5daefd086dfd37f49f48db8993c6c1b5c67073
AlthofZ <althof.edu@gmail.com>
2024-05-20 15:35:33 +0700

    make pass

--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useInput from '../hooks/useInput';
 
-function LoginInput({ }) {
+function LoginInput({ login }) {
   const [email, onEmailChange] = useInput('');
   const [password, onPasswordChange] = useInput('');
 
